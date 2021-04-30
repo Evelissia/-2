f0d48ad91ca375fec2efbd1253f1b7ec4df17eb0
Evelissia <70744852+Evelissia@users.noreply.github.com>
2021-04-30 21:31:15 +0600

    Добавьте файлы проекта.

---
+++ Шоколадка2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Шоколадка2
+{
+    partial class gameForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameView = new System.Windows.Forms.DataGridView();
+            this.btnTwoPlayer = new System.Windows.Forms.Button();
+            this.btnPlayerAndComputer = new System.Windows.Forms.Button();
+            this.aboutMe = new System.Windows.Forms.Button();
+            this.gameRules = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gameView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameView
+            // 
+            this.gameView.AllowUserToAddRows = false;
+            this.gameView.AllowUserToDeleteRows = false;
+            this.gameView.AllowUserToResizeColumns = false;
+            this.gameView.AllowUserToResizeRows = false;
+            this.gameView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gameView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gameView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gameView.ColumnHeadersVisible = false;
+            this.gameView.Location = new System.Drawing.Point(12, 12);
+            this.gameView.Name = "gameView";
+            this.gameView.ReadOnly = true;
+            this.gameView.RowHeadersVisible = false;
+            this.gameView.RowHeadersWidth = 30;
+            this.gameView.Size = new System.Drawing.Size(175, 178);
+            this.gameView.TabIndex = 0;
+            this.gameView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gameView_CellClick);
+            // 
+            // btnTwoPlayer
+            // 
+            this.btnTwoPlayer.Location = new System.Drawing.Point(193, 12);
+            this.btnTwoPlayer.Name = "btnTwoPlayer";
+            this.btnTwoPlayer.Size = new System.Drawing.Size(107, 40);
+            this.btnTwoPlayer.TabIndex = 1;
+            this.btnTwoPlayer.Text = "Игрок против игрока";
+            this.btnTwoPlayer.UseVisualStyleBackColor = true;
+            this.btnTwoPlayer.Click += new System.EventHandler(this.btnTwoPlayer_Click);
+            // 
+            // btnPlayerAndComputer
+            // 
+            this.btnPlayerAndComputer.Location = new System.Drawing.Point(193, 58);
+            this.btnPlayerAndComputer.Name = "btnPlayerAndComputer";
+            this.btnPlayerAndComputer.Size = new System.Drawing.Size(107, 40);
+            this.btnPlayerAndComputer.TabIndex = 2;
+            this.btnPlayerAndComputer.Text = "Игрок против Компьютера";
+            this.btnPlayerAndComputer.UseVisualStyleBackColor = true;
+            this.btnPlayerAndComputer.Click += new System.EventHandler(this.btnPlayerAndComputer_Click);
+            // 
+            // aboutMe
+            // 
+            this.aboutMe.Location = new System.Drawing.Point(193, 150);
+            this.aboutMe.Name = "aboutMe";
+            this.aboutMe.Size = new System.Drawing.Size(107, 40);
+            this.aboutMe.TabIndex = 3;
+            this.aboutMe.Text = "Об Авторе";
+            this.aboutMe.UseVisualStyleBackColor = true;
+            this.aboutMe.Click += new System.EventHandler(this.aboutMe_Click);
+            // 
+            // gameRules
+            // 
+            this.gameRules.Location = new System.Drawing.Point(193, 104);
+            this.gameRules.Name = "gameRules";
+            this.gameRules.Size = new System.Drawing.Size(107, 40);
+            this.gameRules.TabIndex = 4;
+            this.gameRules.Text = "Правила игры";
+            this.gameRules.UseVisualStyleBackColor = true;
+            this.gameRules.Click += new System.EventHandler(this.gameRules_Click);
+            // 
+            // gameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 227);
+            this.Controls.Add(this.gameRules);
+            this.Controls.Add(this.aboutMe);
+            this.Controls.Add(this.btnPlayerAndComputer);
+            this.Controls.Add(this.btnTwoPlayer);
+            this.Controls.Add(this.gameView);
+            this.Name = "gameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Игра Шоколадка";
+            ((System.ComponentModel.ISupportInitialize)(this.gameView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gameView;
+        private System.Windows.Forms.Button btnTwoPlayer;
+        private System.Windows.Forms.Button btnPlayerAndComputer;
+        private System.Windows.Forms.Button aboutMe;
+        private System.Windows.Forms.Button gameRules;
+    }
+}
+
---
+++ Шоколадка2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Шоколадка2
+{
+    public partial class gameForm : Form
+    {        
+        static int row = 8;
+        static int column = 8;
+       
+        static bool isComputer = false;
+        static bool isComputerFirstMove = true;
+        static int computerI;
+        static int computerJ;
+
+        public gameForm()
+        {
+            InitializeComponent();        
+            gameView.RowCount = row;
+            gameView.ColumnCount = column;
+            // Закрашиваем отравленную дольку
+            gameView.Rows[row - 1].Cells[0].Style.BackColor = Color.Red;
+        }
+
+        private static void ComputerMove (object gameView)
+        {
+            Application.DoEvents();
+            Thread.Sleep(300); // ждем 300 мс
+            int computerX = 0;
+            int computerY = 0;
+            // если ход компьютера и игрок ещё не нажал на ячейку,
+            // следующую по диагонали от ядовитой, то на нее нажимает компьютер
+            if (isComputerFirstMove && computerI != row - 2 && computerJ != 1)
+            {
+                computerX = 1;
+                computerY = row - 2;
+                isComputerFirstMove = false;
+            }
+            else if (isComputerFirstMove && computerI == 5 || computerI == 4 || computerI == 3 || computerI == 2 || computerI == 1 || computerI == 0 && computerJ == 1)
+            {
+                computerX = 1;
+                computerY = row - 2;
+                isComputerFirstMove = false;
+
+                // Закрашивание зеркальных ячеек
+                if (((DataGridView)gameView)[computerX, computerY].Style.BackColor == Color.Pink)
+                {
+                    computerX = column - computerI - 1;
+                    computerY = row - computerJ - 1;
+                }
+            }
+            else if (isComputerFirstMove && computerI == 6 && computerJ == 7 || computerJ == 6 || computerJ == 5 || computerJ == 4 || computerJ == 3 || computerJ == 2)
+            {
+                computerX = 1;
+                computerY = row - 2;
+                isComputerFirstMove = false;
+
+                // Закрашивание заркальных ячеек
+                if (((DataGridView)gameView)[computerX, computerY].Style.BackColor == Color.Pink)
+                {
+                    computerX = column - computerI - 1;
+                    computerY = row - computerJ - 1;
+                }
+            }
+
+            // если ход компьютера и игрок нажал на кнопку [6,0] то компьютер нажимает на [7,1]
+            else if (isComputerFirstMove && computerI == 6 && computerJ == 0)
+            {
+                computerX = 1;
+                computerY = 7;
+                isComputerFirstMove = false;
+            }
+            else if (isComputerFirstMove && computerI == 7 && computerJ == 1)
+            {
+                computerX = 0;
+                computerY = row - 2;
+                isComputerFirstMove = false;
+            }
+            // если вторая по диагонали от красной ячейка занята, но сейчас ход компьютера
+            // выбираем левую верхнюю ячейку
+            else if (isComputerFirstMove)
+            {
+                computerX = 0;
+                computerY = 0;
+                isComputerFirstMove = false;
+            }                       
+            // иначе выбираем такую ячейку, которая зеркальна нажатой ячейке
+            else
+            {
+                computerX = column - computerI - 1;
+                computerY = row - computerJ - 1;
+
+                // если зеркальная ячейка занята - занимаем следующую
+                if (((DataGridView)gameView)[computerX, computerY].Style.BackColor == Color.Pink)
+                {
+                    if (computerX == column - 1)
+                        computerX++;
+                    else
+                        computerY++;                  
+                }
+                // если компьютер нажал отравленную ячейку - выводим сообщение
+                if (computerY == 7 && computerX == 0)
+                {
+                    MessageBox.Show("Вы победили!");
+                }
+          
+                isComputerFirstMove = false;
+            }
+            // проверка на выход за пределы
+            if (computerX == column || computerY == row)
+                return;
+            for (int y = 0; y <= computerY; y++)
+                for (int x = computerX; x < column; x++)
+                    ((DataGridView)gameView)[x, y].Style.BackColor = Color.Pink;            
+        }
+
+        private void gameView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = e.RowIndex, j = e.ColumnIndex;
+            
+            for (int y = 0; y <= i; y++)
+                for (int x = j; x < column; x++)
+                    // Закрашиваем все ячейки находящиеся выше и правее активной
+                    gameView[x, y].Style.BackColor = Color.Pink;
+
+            if (isComputer)
+            {
+                // если играем с компьютером
+                // запоминаем куда нажал игрок
+                computerI = i;
+                computerJ = j;
+                // создаем новый поток для реалистичности
+                Thread computerMove = new Thread(ComputerMove);
+                computerMove.Start(gameView);   
+                
+            }
+
+            Win();
+        }
+
+        private void Win()
+        {
+            // Задаем переменную, которая будет хранить активную ячейку
+            DataGridViewCell poison = gameView.CurrentCell;
+
+            // Присваиваем переменной poison конкретную ячейку и при ее активации выводим сообщение
+            if (poison == gameView.Rows[row - 1].Cells[0])
+            {
+                MessageBox.Show("Вы проиграли!");
+            }
+        }
+
+        // Обработчик кнопки Игрок против Игрока
+        private void btnTwoPlayer_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                    // Создаем новую игру и перекрашиваем ячейки обратно в белый
+                    gameView[i, j].Style.BackColor = Color.Empty;
+
+            // Закрашиваем отравленную дольку
+            gameView.Rows[row - 1].Cells[0].Style.BackColor = Color.Red;
+
+            isComputer = false;
+        }
+        // Обработчик кнопки Игрок против компьютера
+        private void btnPlayerAndComputer_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < row; x++)
+                for (int y = 0; y < column; y++)
+                    // Создаем новую игру и перекрашиваем ячейки обратно в белый
+                    gameView[x, y].Style.BackColor = Color.Empty;
+
+            // Закрашиваем отравленную дольку
+            gameView.Rows[row - 1].Cells[0].Style.BackColor = Color.Red;
+
+            isComputer = true;
+            isComputerFirstMove = true;
+        }
+
+        private void gameRules_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поле игры «Шоколадка» — прямоугольная плитка шоколада; " +
+                            "двое игроков по очереди выбирают одну дольку и съедают вместе со всеми дольками выше и правее её. " +
+                            "Проигрывает тот игрок, который съедает «отравленную» левую нижнюю дольку.", "Правила игры");
+        }
+
+        private void aboutMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Половцева Виктория Владимировна\n" +
+                            "ОмГТУ. Группа: ПИН-202\n" +
+                            "Факультет информационных технологий и компьютерных систем\n" +
+                            "Кафедра Автоматизированные системы обработки информации и управления", "Об Авторе");
+        }
+    }
+}
+
+    
